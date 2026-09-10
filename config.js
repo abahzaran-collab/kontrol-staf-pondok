67e0abcd100326f8a1062225f20aad27471b6569
abahzaran-collab <abahzaran@gmail.com>
2026-09-10 10:24:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Dari Supabase > Project Settings > API
-const SUPABASE_URL = 'PASTE_PROJECT_URL';
-const SUPABASE_ANON_KEY = 'PASTE_PUBLISHABLE_OR_ANON_KEY';
+const SUPABASE_URL = 'https://kxnhsiduuhrnbxucmzju.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_b6rp_hI-fYs4j28uJ_SajA_VG-y1CHR'
 // Jangan pernah memasukkan service_role/secret key ke file ini.
